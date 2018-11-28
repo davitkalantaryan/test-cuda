d52c3b47c1eac09696e4f0fd05ca36e710aeb236
Davit Kalantaryan <davit.kalantaryan1980@gmail.com>
2018-11-28 07:57:11 +0100

    first commit

---
+++ include/first_include_for_cuda_softs.h
@@ -0,0 +1,12 @@
+//
+// file:		first_include_for_cuda_softs.h
+// created on:	2018 Nov 27
+// created by:	D. Kalantaryan (davit.kalantaryan@desy.de)
+//
+
+#ifndef __first_include_for_cuda_softs_h__
+#define __first_include_for_cuda_softs_h__
+
+
+
+#endif  // #ifndef __first_include_for_cuda_softs_h__
---
+++ src/test/main_cuda_test1.cpp
@@ -0,0 +1,12 @@
+//
+// file:		main_cuda_test1.cpp
+// created on:	2018 Nov 27
+// created by:	D. Kalantaryan (davit.kalantaryan@desy.de)
+//
+
+#include <first_include_for_cuda_softs.h>
+
+int main()
+{
+	return 0;
+}
